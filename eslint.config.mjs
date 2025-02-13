8446b1c791cc3958d2705732e1eb63670b29e706
Sylvia Wang <sylviamiaowang@gmail.com>
2025-02-13 20:51:05 +1100

    fix eslint config???

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,7 @@
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import reactPlugin from "eslint-plugin-react";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [
-    {
-      ignores: ["node_modules/", "dist/"],
-    },
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      languageOptions: {
-        parser: tsParser,
-        parserOptions: {
-          ecmaVersion: "latest",
-          sourceType: "module",
-        },
-      },
-      plugins: {
-        "@typescript-eslint": tsPlugin,
-        react: reactPlugin,
-      },
-      rules: {
-        "no-unused-vars": "warn",
-        "no-console": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-      },
-    },
-  ];
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
